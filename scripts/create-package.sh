2292ec6c5d270b27f86d400359f218e8f1321135
Vincent den Boer <vincent@shishkabab.net>
2019-06-06 10:43:08 +0200

    Update submodules

--- scripts/create-package.sh
+++ scripts/create-package.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 cd packages || exit 1
-mkdir $1
-cp storex/{License,package.json,tsconfig.json,.gitignore} $1
-mkdir $1/ts
-touch $1/ts/index.ts
-cd $1
+package_dir="@worldbrain/$1"
+mkdir $package_dir
+cp @worldbrain/storex/{License,package.json,tsconfig.json,.gitignore} $package_dir
+mkdir $package_dir/ts
+touch $package_dir/ts/index.ts
+cd $package_dir
 yarn
 
 echo "================="
