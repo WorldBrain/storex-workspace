eb02b8a82d31030a2abeb45e530869462b06a831
Vincent den Boer <vincent@shishkabab.net>
2019-07-21 13:25:49 +0200

    Implement memory access tokens

--- apps/storex-client/ts/public-api.ts
+++ apps/storex-client/ts/public-api.ts
@@ -1,38 +1,49 @@
 import { CollectionDefinitionMap } from '@worldbrain/storex/lib/types'
 
 export interface StorexClientAPI_v0 {
-    registerApp : (name : string) => Promise<{ success : false, errorCode : number, errorText : string } | { success : true, accessToken : string }>
-    identifyApp : (name : string, options : { accessToken : string }) => Promise<{}>
-    // unidentifyApp : () => Promise<void>
+    registerApp(options : { name : string }) : Promise<RegisterAppResult_v0>
+    identifyApp(options : IdentifyAppOptions_v0) : Promise<{}>
+    // unidentifyApp() : Promise<void>
     
-    executeOperation : (options : { operation: any[] }) => Promise<{ result : any }>
+    executeOperation(options : { operation: any[] }) : Promise<{ result : any }>
     
-    // requestPriviliges : (options : {  }) => Promise<{}>
-    // listPrivileges : () => Promise<{}>
+    // requestPriviliges(options : {  }) : Promise<{}>
+    // listPrivileges() : Promise<{}>
     
-    updateCollectionRegistry : (options : { collectionDefinitions : CollectionDefinitionMap }) => Promise<{ success: true }>
-    // describeCollectionRegistry : () => Promise<{}> // In terms of RDF
-    // updateAccessRules : () => Promise<{}>
+    updateSchema(options : UpdateCollectionRegistryOptions_v0) : Promise<{ success: true }>
+    // describeSchema() : Promise<{}> // In terms of RDF
+    // updateAccessRules() : Promise<{}>
 
-    // startMigration : () => Promise<{}>
-    // getMigrationStatus : () => Promise<{}>
+    // startMigration() : Promise<{}>
+    // getMigrationStatus() : Promise<{}>
 
-    // exportData : () => Promise<{}>
-    // importData : (source : string, options : { mode : 'merge' | 'overwrite' }) => Promise<{}>
+    // exportData() : Promise<{}>
+    // importData(source : string, options : { mode : 'merge' | 'overwrite' }) : Promise<{}>
 
-    // storeSecret : () => Promise<{}>
-    // getSecret : () => Promise<{}>
-    // deleteSecret : () => Promise<{}>
+    // storeSecret() : Promise<{}>
+    // getSecret() : Promise<{}>
+    // deleteSecret() : Promise<{}>
 
-    // storeApplicationConfig : () => Promise<{}>
-    // getApplicationConfig : () => Promise<{}>
+    // storeApplicationConfig() : Promise<{}>
+    // getApplicationConfig() : Promise<{}>
 }
+
+export type RegisterAppResult_v0 = { success : false, errorCode : number, errorText : string } | { success : true, accessToken : string }
 
 export enum RegisterAppError_v0 {
     APP_ALREADY_EXISTS = 0
 }
 
+export interface IdentifyAppOptions_v0 {
+    name : string
+    accessToken : string
+}
+
 export enum IdentifyAppError_v0 {
     INVALID_ACCESS_TOKEN = 0,
     DUPLICATE_IDENTFICATION = 1
+}
+
+export interface UpdateCollectionRegistryOptions_v0 {
+    collectionDefinitions : CollectionDefinitionMap
 }
--- apps/storex-client/ts/sessions.ts
+++ apps/storex-client/ts/sessions.ts
@@ -1,13 +1,29 @@
-export class SessionManager {
-    async openSession(appIdentifier : string) {
+import * as api from "./public-api";
+import { CollectionDefinitionMap } from "@worldbrain/storex";
+import { AccessTokenManager } from "./access-tokens";
+
+export interface SessionOptions {
+    accessTokenManager : AccessTokenManager
+}
+export class Session implements api.StorexClientAPI_v0 {
+    constructor(private options : SessionOptions) {
 
     }
 
-    async executeOperation(appIdentifier : string, ...args : any[]) {
+    async registerApp(options : { name : string }) : Promise<api.RegisterAppResult_v0> {
+        const accessToken = await this.options.accessTokenManager.createToken(`app:${options.name}`)
+        return { success: true, accessToken }
+    }
+
+    async identifyApp(options : api.IdentifyAppOptions_v0 ) : Promise<{}> {
+        return {}
+    }
+
+    async executeOperation(options : { operation: any[] }) : Promise<{ result : any }> {
 
     }
 
-    async closeSession(appIdentifier : string) {
+    async updateSchema(options : { collectionDefinitions : CollectionDefinitionMap }) : Promise<{ success: true }> {
 
     }
 }
